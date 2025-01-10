72438d7c43b7c0df6e6c1b7253dd22d66b12ba24
rishyym0927 <rishyym0927@gmail.com>
2025-01-10 21:49:10 +0530

    Updated Server.js file

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
-
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const cryptoRoutes = require('./routes/cryptoRoutes');
-const fetchCryptoData = require('./utils/FetchCryptoData');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const cryptoRoutes = require("./routes/cryptoRoutes");
+const fetchCryptoData = require("./utils/FetchCryptoData");
+const cors = require("cors");
+const helmet = require("helmet");
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 connectDB();
 app.use(express.json());
 app.use(cors());
 app.use(
-    helmet.contentSecurityPolicy({
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://vercel.live"],
-        connectSrc: ["'self'", "https://vercel.live"],
-      },
-    })
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: [
+        "'self'",
+        "'unsafe-inline'",
+        "'unsafe-eval'",
+        "https://vercel.live",
+      ],
+      connectSrc: ["'self'", "https://vercel.live"],
+    },
+  })
+);
+app.use("/api", cryptoRoutes);
+app.get("/", (req, res) => {
+  res.send(
+    "Welcome to the Crypto API and  stats: /api/stats?coin=bitcoin,deviation: /api/deviation?coin=bitcoin"
   );
-app.use('/api', cryptoRoutes);
-app.get('/', (req, res) => {
-    res.send('Welcome to the Crypto API');
 });
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-    fetchCryptoData();
+  console.log(`Server running on port ${port}`);
+  fetchCryptoData();
 });
